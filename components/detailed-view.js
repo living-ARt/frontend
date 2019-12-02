4bb2a4d3720cc1d8e8ac3b6410868fe9fe5f5043
Joy Wang <w.joy5015@gmail.com>
2019-12-02 11:00:48 -0500

    feat: detailed view styling added

--- components/detailed-view.js
+++ components/detailed-view.js
@@ -3,7 +3,8 @@ import {
   StyleSheet,
   Text,
   Image,
-  View,
+  SafeAreaView,
+  ScrollView,
   TouchableOpacity
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
@@ -27,15 +28,17 @@ export default class DetailedView extends Component {
 
   render(){
     return(
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+    <ScrollView>
       <Text style={styles.body}>Artist: {this.state.currentArt.artist}</Text>
       <Text style={styles.body}>Date: {this.state.currentArt.date}</Text>
       <Text style={styles.description}>{this.state.currentArt.description}</Text>
       <Image
-        source={this.state.currentArt.imageUrl}
-        style={{width: 30, height: 30}}
+        source={{uri: this.state.currentArt.imageUrl}}
+        style={styles.img}
       />
-    </View>
+    </ScrollView>
+    </SafeAreaView>
     )
   }
 }
@@ -45,19 +48,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#181C22',
-    paddingRight: 30,
-    paddingLeft: 30
   },
   body: {
     fontSize: 20,
     color: '#fff',
     paddingTop: 20,
-    alignSelf: 'flex-start'
+    alignSelf: 'flex-start',
+    paddingHorizontal: 20
   },
   description: {
     fontSize: 16,
     color: '#fff',
     paddingTop: 20,
-    alignSelf: 'flex-start'
+    alignSelf: 'flex-start',
+    paddingHorizontal: 20,
+    marginBottom: 10
   },
+  img: {
+    width: 350,
+    height: 350,
+    alignSelf: "center",
+    paddingHorizontal: 20
+  }
 })
