68b4441b16ee977c6ccd1a44bbb4ca89f185652b
Natalia Gabuaeva <ngabuaeva@gmail.com>
2019-12-03 14:17:59 -0500

    deleted something accidentally fixed

---
+++ js/SoloPaintingAR.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import {
+  ViroARScene,
+  ViroARSceneNavigator,
+  ViroConstants,
+  ViroARTrackingTargets,
+  ViroARPlane,
+  ViroText,
+  ViroImage,
+  ViroFlexView,
+  ViroARImageMarker,
+  ViroAnimatedImage,
+  ViroAnimations,
+  ViroNode,
+  ViroSound
+
+} from 'react-viro';
