106fd2df83e2843835968aaf6f44de847859c6f1
Natalia Gabuaeva <ngabuaeva@gmail.com>
2019-12-04 12:22:19 -0500

    not working

--- js/HelloWorldScene.js
+++ js/HelloWorldScene.js
@@ -9,24 +9,35 @@ import {
   ViroVideo,
   ViroText,
   Viro360Video,
+  ViroAnimations,
+  ViroAnimatedImage
 } from 'react-viro';
 
 export default class HelloWorldScene extends Component {
 
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
+    this.video = this.props.sceneNavigator.viroAppProps
     this.state = {} // Set initial state here
   }
 
   render() {
+    console.log('props from vr:', this.props.sceneNavigator.viroAppProps)
     return (
       <ViroScene>
-        <Viro360Video
-          source={require('../assets/video/Cypresses.mp4')}
+        <ViroAnimatedImage
+          resizeMode={'StretchToFill'}
+          // scale={[10, 10, 10]}
+          position={[0, 0, -10]}
+          visible={true}
+          animation={{
+            name: 'animateImage',
+            run: true,
+            loop: true,
+          }}
           loop={true}
-          position={[0, 0, -2]}
-          scale={[5, 3, 3]}
+          rotation={[-90, 0, 0]}
+          source={{ uri: `https://living-art-animations.s3.amazonaws.com/sunflowers_360.gif` }}
         />
       </ViroScene>
     );
@@ -43,5 +54,13 @@ var styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
+
+ViroAnimations.registerAnimations({
+  animateImage: {
+    properties: {
+      opacity: 1.0
+    }
+  },
+})
 
 module.exports = HelloWorldScene;
